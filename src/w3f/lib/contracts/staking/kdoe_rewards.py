ad3450cf24fb5413aa1b587c2e9ef044384a273d
ipdoe <idoecrypto@protonmail.com>
2023-03-04 21:53:17 +0100

    Add KDOERwards contract and wealth script

---
+++ src/w3f/lib/contracts/staking/kdoe_rewards.py
@@ -0,0 +1,86 @@
+from web3 import Web3
+from  w3f.lib.contracts.staking import kdoe_rewards_abi
+import requests, datetime, json
+
+contract_address = "0xF541Ab2f4DBa2071B12DA99a9Ee83B0C4b719d95"
+CREATION_BLOCK = 16395899
+
+def get_balance(w3, wallet, block_identifier='latest'):
+    contract = w3.eth.contract(address=contract_address, abi=kdoe_rewards_abi.get_abi())
+    balanceOf =  Web3.fromWei(contract.functions.balanceOf(wallet).call(block_identifier=block_identifier), "ether")
+    earned = Web3.fromWei(contract.functions.earned(wallet).call(block_identifier=block_identifier), "ether")
+
+    return {
+        "Staked": balanceOf,
+        "Rewards": earned,
+        }
+
+def all_doe_stake_transactions_since(api_key, start_block):
+    data = {
+        'module': 'account',
+        'action': 'txlist',
+        'address': contract_address,
+        'startblock': str(start_block),
+        'endblock':'99999999',
+        'sort': 'asc',
+        'apikey': api_key,
+    }
+    return requests.get("https://api.etherscan.io/api", data=data).json()['result']
+
+def all_doe_stake_transactions(api_key):
+    return all_doe_stake_transactions_since(api_key, CREATION_BLOCK)
+
+def dump_all_stakers_since(api_key, start_block):
+    addresses = []
+    dump = all_doe_stake_transactions_since(api_key, start_block)
+    for tx in dump:
+        addr = Web3.toChecksumAddress(tx['from'])
+        if addr not in addresses:
+            addresses.append(addr)
+
+    return addresses
+
+def dump_all_stakers(api_key):
+    return dump_all_stakers_since(api_key, CREATION_BLOCK)
+
+def get_all_balances(w3, addr_list, block_identifier='latest'):
+    holders_lst = []
+    hodlers = {}
+    for addr in addr_list:
+        balance = get_balance(w3, addr, block_identifier)
+        total = float(balance['Staked'] + balance['Rewards'])
+        if (total > 0.0):
+            balance_summary = [addr, total, float(balance['Staked']), float(balance['Rewards'])]
+            holders_lst.append(balance_summary)
+    holders_lst.sort(key=lambda x: x[1], reverse=True)
+
+    for hodler in holders_lst:
+        hodlers[hodler[0]] = {"total": hodler[1], "staked": hodler[2], "rewards": hodler[3]}
+    return hodlers
+
+def get_sorted_balances(w3, addr_list, block_identifier='latest'):
+    print(len(addr_list))
+    holders_lst = []
+    hodlers = {}
+    cnt = 0
+    for addr in addr_list:
+        balance = get_balance(w3, addr, block_identifier)
+        total = float(balance['Staked'] + balance['Rewards'])
+        if (cnt % 20) == 0: print(cnt)
+        if (total > 0.0):
+            balance_summary = [addr, total, float(balance['Staked']), float(balance['Rewards'])]
+            holders_lst.append(balance_summary)
+        cnt = cnt + 1
+    holders_lst.sort(key=lambda x: x[1], reverse=True)
+
+    for hodler in holders_lst:
+        hodlers[hodler[0]] = {"total": hodler[1], "staked": hodler[2], "rewards": hodler[3]}
+    return hodlers
+
+def dump_hodlers_json(path, hodlers):
+    # todo: last comma in the file is not json compatible
+    with open(path, 'w') as w:
+        w.write("{")
+        for hodler in hodlers:
+            w.write(f'\"{hodler}\": {json.dumps(hodlers[hodler])},\n')
+        w.write("}\n")
---
+++ tests/test_kdoe_rewards.py
@@ -0,0 +1,15 @@
+
+import json, os
+from venv import create
+from w3f.lib.contracts.staking import kdoe_rewards
+from w3f.lib import logger as log
+import w3f.hidden_details as hd
+
+from web3 import Web3
+
+staking_wallet = "0x7eD309643FB211e846a6D0A73Cd384e2e7A67B69"
+W3 = Web3(Web3.HTTPProvider(hd.eth_mainnet))
+
+def test__get_balance():
+    balance = kdoe_rewards.get_balance(W3, staking_wallet)
+    log.log(balance)
